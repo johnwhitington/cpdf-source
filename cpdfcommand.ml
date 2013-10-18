b9dbd3d367343f3b369ef98efb469c982a14c7e1
John Whitington <john@coherentgraphics.co.uk>
2013-10-18 11:55:39 +0100

    Version number

--- cpdfcommand.ml
+++ cpdfcommand.ml
@@ -3,7 +3,7 @@ let demo = false
 let noncomp = false
 let major_version = 1
 let minor_version = 8
-let version_date = "(unreleased)"
+let version_date = "(unreleased, 18th Oct 2013)"
 
 open Pdfutil
 open Pdfio
