96678975742c60131ee9ed1eeadca9fcacf632fc
John Whitington <john@coherentgraphics.co.uk>
2017-11-09 18:15:51 +0000

    more

--- cpdfcommand.ml
+++ cpdfcommand.ml
@@ -3,7 +3,7 @@ let demo = false
 let noncomp = false
 let major_version = 2
 let minor_version = 3
-let version_date = "(devel, build of 4th November 2017)"
+let version_date = "(devel, build of 5th November 2017)"
 
 open Pdfutil
 open Pdfio
