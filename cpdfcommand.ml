28cba5dbdf80fa8e0122f78a522e8ce2c250705a
John Whitington <john@coherentgraphics.co.uk>
2021-06-20 16:27:46 +0100

    install script

--- cpdfcommand.ml
+++ cpdfcommand.ml
@@ -3,7 +3,7 @@ let demo = false
 let noncomp = false
 let major_version = 2
 let minor_version = 4
-let version_date = "(devel, 16th February 2020)"
+let version_date = "(18th June 2021)"
 
 open Pdfutil
 open Pdfio
