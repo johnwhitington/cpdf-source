7b1dadf7766cf9ceedffd030b3552c1bf4a091e0
John Whitington <john@coherentgraphics.co.uk>
2023-05-09 15:02:59 +0100

    Newly-created files get IDs by default

--- cpdfcreate.ml
+++ cpdfcreate.ml
@@ -5,11 +5,15 @@ let blank_document width height pages =
     map (fun () -> Pdfpage.blankpage (Pdfpaper.make Pdfunits.PdfPoint width height)) (many () pages)
   in
     let pdf, pageroot = Pdfpage.add_pagetree pdf_pages (Pdf.empty ()) in
-      Pdfpage.add_root pageroot [] pdf
+    let pdf = Pdfpage.add_root pageroot [] pdf in
+      Pdf.change_id pdf (string_of_float (Random.float 1.));
+      pdf
 
 let blank_document_paper papersize pages =
   let pdf_pages =
     map (fun () -> Pdfpage.blankpage papersize) (many () pages)
   in
     let pdf, pageroot = Pdfpage.add_pagetree pdf_pages (Pdf.empty ()) in
-      Pdfpage.add_root pageroot [] pdf
+    let pdf = Pdfpage.add_root pageroot [] pdf in
+      Pdf.change_id pdf (string_of_float (Random.float 1.));
+      pdf
