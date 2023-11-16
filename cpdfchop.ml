ddf149254edb499119c765b3e3ca8a96170a3841
John Whitington <john@coherentgraphics.co.uk>
2023-11-16 18:18:11 +0000

    Design comments

--- cpdfchop.ml
+++ cpdfchop.ml
@@ -1,5 +1,12 @@
 open Pdfutil
 open Cpdferror
 
+(* Prevent duplication of content by sharing content streams *)
+(* Resources - patterns etc. - pattern matrices *)
+(* Bookmarks: merge/duplicate them *)
+(* Annotations: merge/duplicate them *)
+(* Page boxes - mediabox or cropbox used? Output modifies all boxes? *)
+(* fast/slow *)
+
 (* Chop pages into pieces *)
 let chop ~x ~y pdf range = pdf
