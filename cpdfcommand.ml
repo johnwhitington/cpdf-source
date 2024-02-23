1c67f2b806ad8e6786a7d7e54745485bbf660170
John Whitington <john@coherentgraphics.co.uk>
2024-02-23 18:33:59 +0000

    Version number and pagination

--- cpdfcommand.ml
+++ cpdfcommand.ml
@@ -3,7 +3,7 @@ let demo = false
 let noncomp = false
 let major_version = 2
 let minor_version = 7
-let version_date = "(20th February 2024, Beta 1)"
+let version_date = "(23rd February 2024)"
 
 open Pdfutil
 open Pdfio
