70c6fe9b638354e68439c4d1bcfa24e8a646ec36
John Whitington <john@coherentgraphics.co.uk>
2024-07-24 15:05:02 +0100

    Docstrings

--- cpdfprinttree.mli
+++ cpdfprinttree.mli
@@ -1,5 +1,7 @@
 (** Print trees *)
 
+(** Print a tree to a buffer. *)
 val to_buffer : ?line_prefix: string -> get_name: ('a -> string) -> get_children: ('a -> 'a list) -> Buffer.t -> 'a -> unit
 
+(** Print a tree to a string. *)
 val to_string : ?line_prefix: string -> get_name: ('a -> string) -> get_children: ('a -> 'a list) -> 'a -> string
--- cpdfua.mli
+++ cpdfua.mli
@@ -1,16 +1,25 @@
 (** PDF/UA *)
+
+(** Print matterhorn test results ("" = all tests) *)
 val test_matterhorn_print : Pdf.t -> string -> unit
 
+(** Return JSON results of a matterhorn test ("" = all tests *) 
 val test_matterhorn_json : Pdf.t -> string -> Cpdfyojson.Safe.t
 
+(** Mark PDF as PDF/UA-1 compliant. *)
 val mark : Pdf.t -> unit
 
+(** Mark PDF as PDF/UA-2 compliant. *)
 val mark2 : int -> Pdf.t -> unit
 
+(** Remove any PDF/UA-* marker *)
 val remove_mark : Pdf.t -> unit
 
+(** Print the structure tree to standard output, graphically. *)
 val print_struct_tree : Pdf.t -> unit
 
+(** Extract structure tree to JSON *)
 val extract_struct_tree : Pdf.t -> Cpdfyojson.Safe.t
 
+(** Reapply an edited JSON structure tree to its PDF. *)
 val replace_struct_tree : Pdf.t -> Cpdfyojson.Safe.t -> unit
