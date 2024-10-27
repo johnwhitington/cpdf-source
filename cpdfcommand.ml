997b32b44030908e502b5a9c8f8cfc1ea279d452
John Whitington <john@coherentgraphics.co.uk>
2024-10-27 21:12:01 +0000

    Version

--- cpdfcommand.ml
+++ cpdfcommand.ml
@@ -3,8 +3,8 @@ let demo = false
 let agpl = true
 let major_version = 2
 let minor_version = 7
-let minor_minor_version = 1
-let version_date = "(24th July 2024)"
+let minor_minor_version = 2
+let version_date = "(28th October 2024)"
 
 open Pdfutil
 open Pdfio
