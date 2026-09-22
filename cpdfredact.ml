3f0f123535159e6bc12c7acb13dc7efa5fe1f901
John Whitington <john@coherentgraphics.co.uk>
2026-09-22 17:19:42 +0100

    Finished -redact-apply-appearance support

--- cpdfredact.ml
+++ cpdfredact.ml
@@ -227,29 +227,30 @@ in the redaction annotations. Since redaction annotations are generally only
 created by modern PDF implementations, it's reasonable to assume this exists.
 In the future, when we support more kinds of annotation, we'll have to add
 appearance stream generation/regeneration. *)
-(* FIXME: Pull out names_used internals from Pdfpage to make a version just for /Resources/XObjects and fit it in here. *)
 let stamp_annotation_appearance pdf page i =
-  flprint (Pdfwrite.string_of_pdf (Pdf.direct pdf (Pdf.Indirect i)));
-  match Pdf.lookup_immediate "/RO" (Pdf.direct pdf (Pdf.Indirect i)) with
-  | Some (Pdf.Indirect ro) ->
-      let xobjects =
-        match Pdf.lookup_direct pdf "/XObject" page.Pdfpage.resources with
-        | Some d -> d
-        | None -> Pdf.Dictionary []
-      in
-        let resources = Pdf.add_dict_entry page.Pdfpage.resources "/XObject" (Pdf.add_dict_entry xobjects "/NewName" (Pdf.Indirect ro)) in
-        let matrix = Pdftransform.matrix_invert (Pdf.parse_matrix pdf "/Matrix" (Pdf.direct pdf (Pdf.Indirect ro))) in
-        let ops = Pdfops.parse_operators pdf page.Pdfpage.resources page.Pdfpage.content in
-        let ops = Pdfops.Op_q::Pdfops.Op_cm matrix::Pdfops.Op_Do "/NewName"::Pdfops.Op_Q::ops in
-          {page with resources; content = [Pdfops.stream_of_ops ops]}
-  | _ -> page
+  let rec fresh_name ns n =
+    let newname = "/X" ^ string_of_int n in
+    if mem newname ns then fresh_name ns (n + 1) else newname
+  in
+    match Pdf.lookup_immediate "/RO" (Pdf.direct pdf (Pdf.Indirect i)) with
+    | Some (Pdf.Indirect ro) ->
+        let xobjects, name =
+          match Pdf.lookup_direct pdf "/XObject" page.Pdfpage.resources with
+          | Some (Pdf.Dictionary d) -> (Pdf.Dictionary d, fresh_name (map fst d) 0)
+          | _ -> (Pdf.Dictionary [], "/X0")
+        in
+          let resources = Pdf.add_dict_entry page.Pdfpage.resources "/XObject" (Pdf.add_dict_entry xobjects name (Pdf.Indirect ro)) in
+          let matrix = Pdftransform.matrix_invert (Pdf.parse_matrix pdf "/Matrix" (Pdf.direct pdf (Pdf.Indirect ro))) in
+          let ops = Pdfops.parse_operators pdf page.Pdfpage.resources page.Pdfpage.content in
+          let ops = Pdfops.Op_q::Pdfops.Op_cm matrix::Pdfops.Op_Do name::Pdfops.Op_Q::ops in
+            {page with resources; content = [Pdfops.stream_of_ops ops]}
+    | _ -> page
 
 (* Apply redaction annotations. *)
 let apply
   pdf ~appearance ~text_spec ~image_spec ~inline_image_spec ~vector_spec ~annotation_spec ~path_to_jbig2dec
   ~path_to_convert ~path_to_jbig2enc ?(typ="/Redact") ~invert ~show ~color ~outline ~opacity ~linewidth ~underneath range
 =
-  Printf.printf "appearance = %b\n" appearance;
   let show = if appearance then false else show in
   preprocess_jbig2lossy_to_jbig2lossless ~jbig2dec:path_to_jbig2dec ~path_to_jbig2enc pdf;
   let rectangles = ref [] in
